0e9c562af7c36b525e8859a3cfc80b3baeb62c78
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2024-09-26 08:33:18 -0300

    Tiles, validando posições

--- Game_01/src/com/dsabino/world/World.java
+++ Game_01/src/com/dsabino/world/World.java
@@ -1,5 +1,6 @@
 package com.dsabino.world;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -7,7 +8,8 @@ import javax.imageio.ImageIO;
 
 public class World {
 	
-	
+	private Tile[] tiles;
+	public static int WIDTH, HEIGHT;
 	
 	public World(String path) {
 		
@@ -15,10 +17,33 @@ public class World {
 			BufferedImage map = ImageIO.read(getClass().getResource(path));
 			
 			int[] pixels = new int[map.getWidth() * map.getHeight()];
+			WIDTH = map.getWidth();
+			HEIGHT = map.getHeight();
+			
+			
+			tiles = new Tile[map.getWidth() * map.getHeight()];
+			
 			map.getRGB(0, 0, map.getWidth(), map.getHeight(), pixels, 0, map.getWidth());
-			for(int i = 0; i < pixels.length; i++) {
-				if(pixels[i] == 0xFFFF0000) {
-					System.out.println("Estou no pixel vermelho");
+			for ( int xx = 0; xx < map.getWidth(); xx++) {
+				for(int yy = 0; yy < map.getHeight(); yy++) {
+					int pixelAtual = pixels[xx + (yy * map.getWidth())];
+					
+					if(pixelAtual == 0xFF000000 ) {
+						//Floor (Chão)
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+						
+					}else if(pixelAtual == 0xFFFFFFFF ) {
+						//Wall (Parede)
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_WALL);
+						
+					} else if (pixelAtual == 0xFF191DFF) {
+						//Player
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+					}
+					else {
+						//Floor (Chão)
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+					}
 				}
 			}
 			
@@ -26,5 +51,14 @@ public class World {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public void render(Graphics g) {
+		for (int xx = 0; xx < WIDTH; xx++) {
+			for(int yy = 0; yy < HEIGHT; yy++) {
+				Tile tile = tiles[xx + (yy*WIDTH)];
+				tile.render(g);
+			}
+		}
 	}
 }
